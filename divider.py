139729e9eb9a939b91abd38f472e05267c465bc6
Aaryan Patel <patelaaryan2001@gmail.com>
2024-02-01 18:29:46 +0530

    Modify divider.py
    Call fullSubtractor function in sub.py in the divider.py

--- divider.py
+++ divider.py
@@ -1,22 +1,25 @@
 from circuit import CircuitBehaviour
+from sub import SubCircuit
 
 
 class DividerCircuit(CircuitBehaviour):
 
-    def __subtract(self, dividend: int, divisor: int) -> int:
+    def __init__(self):
+        super().__init__()
+        # Create an instance of the SubCircuit class to access its subtraction method
         """
         This function simulates subtraction. It subtracts the divisor from the dividend.
         Here repeated subtraction is done in order to do the division.
         """
-        while dividend >= divisor:
-            dividend -= divisor
-        return dividend
+
+        self.sub_circuit = SubCircuit()
 
     def fire(self, dividend: int, divisor: int) -> int:
         """
         This function defines the configuration of the divider circuit.
-        The function internally uses the self.__subtract function repeatedly
+        The function internally uses the subtraction function from SubCircuit
         to perform integer division using bitwise operations and return the quotient.
+        
         """
         if divisor <= 0:
             raise ValueError("The divisor must be a positive integer")
@@ -28,11 +31,12 @@ class DividerCircuit(CircuitBehaviour):
         remainder = 0
 
         # Assuming the integer bit representation as 32 bit.
-
-        for _ in range(31, -1, -1):
-            remainder = (remainder << 1) | ((dividend >> _) & 1)
+        for bit_position in range(31, -1, -1):
+            remainder = (remainder << 1) | ((dividend >> bit_position) & 1)
             if remainder >= divisor:
-                remainder = self.__subtract(remainder, divisor)
+                # Use the fire method from the SubCircuit class for subtraction
+                diff = self.sub_circuit.fire(remainder, divisor)
+                remainder = diff
                 quotient = (quotient << 1) | 1
             else:
                 quotient = quotient << 1
