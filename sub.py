9f6c6d704fffe1a1f695d40ec462c6659570fd65
Khdave02 <87975841+Khdave02@users.noreply.github.com>
2024-02-01 17:39:03 +0530

    Delete sub.py

--- sub.py
+++
@@ -1,8 +0,0 @@
-from circuit import CircuitBehaviour
-
-class SubCircuit(CircuitBehaviour):    
-    def fire(self, operand1: int, operand2: int) -> (int):
-        '''
-        This function defines the configuration of the ciruit.
-        '''
-        return operand1 - operand2
